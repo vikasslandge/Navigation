784ac26ad6491c4fce73815d6f1b3d5274058e4c
vikasslandge <vikasslandge@gmail.com>
2018-09-08 23:52:54 +0530

    first commit

--- app/src/main/java/com/example/vikaslandge/navigationmenutoobar/randomActivity.kt
+++ app/src/main/java/com/example/vikaslandge/navigationmenutoobar/randomActivity.kt
@@ -2,11 +2,16 @@ package com.example.vikaslandge.navigationmenutoobar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_random.*
+import java.util.*
 
 class randomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
+        val r= Random()
+
+        textView3.text= r.nextInt(15).toString()
     }
 }
